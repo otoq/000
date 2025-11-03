0a20559ca0462ac0350a16f9898efeb61b925780
Hamdi YILMAZ <hamdiyilmaz85@gmail.com>
2025-11-03 15:21:16 +0100

    22

--- game_player.js
+++ game_player.js
@@ -14,7 +14,7 @@ const CONFIG = {
     "https://unblockedgames66.gitlab.io",
     "https://ubgwtf.gitlab.io",
   ],
-  jsonUrl: "https://ucbg.online/data-json/games.json?v=2.0.12",
+  jsonUrl: "/data-json/games.json?v=2.0.14",
   theme: {
     bg: "#0b1220",
     panel: "#0f172a",
